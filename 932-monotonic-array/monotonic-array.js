1d001bddef227871d580922e67b8d540750ec33f
Rudrant Joshi <rudrant.joshi@gmail.com>
2026-08-24 07:21:38 +0530

    Time: 1 ms (93.67%) | Memory: 66.7 MB (71.67%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.js
@@ -0,0 +1,13 @@
+var isMonotonic = function(nums) {
+    let ac = true, dc = true;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < nums[i - 1]) {
+            ac = false;
+        }
+        if(nums[i] > nums[i-1]){
+          dc = false
+        }
+    }
+    return ac || dc
+};
